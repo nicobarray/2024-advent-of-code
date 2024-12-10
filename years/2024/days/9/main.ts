af1a3ec97d801244241cc29d008d504328ece378
Nicolas Barray <nicolas.barray@amblea.fr>
2024-12-10 01:04:31 +0100

    clean file

--- years/2024/days/9/main.ts
+++ years/2024/days/9/main.ts
@@ -1,18 +1,5 @@
 import * as path from "jsr:@std/path@1";
-import { div, sumAll } from "../../../../util.ts";
-import { Buffer } from 'node:buffer'
-import { format } from "jsr:@std/fmt/duration";
-import { sortBy } from "jsr:@std/collections";
-
-const file = await Deno.readTextFile(
-  path.join(import.meta.dirname, "input.txt"),
-);
-
-const bonus = await Deno.readTextFile(
-  path.join(import.meta.dirname, "bonus.txt"),
-);
-
-const example = "2333133121414131402";
+import { sumAll } from "../../../../util.ts";
 
 const range = (n: number) => [...new Array(n).keys()];
 
@@ -60,45 +47,46 @@ export async function step2(input: string) {
   const diskMap = input.split("");
 
   const disk: (string | number)[] = []; // "." | fileId
-  const files: [number, number][] = [] // x, size
-  const freeSlots: [number, number][] = [] // x, size
+  const files: [number, number][] = []; // x, size
+  const freeSlots: [number, number][] = []; // x, size
 
   for (let i = 0; i < diskMap.length; i++) {
     const size = Number(diskMap[i]);
     if (i % 2 === 0) {
-      files.push([disk.length, size])
+      files.push([disk.length, size]);
       range(size).forEach(() => disk.push(i / 2));
     } else {
-      freeSlots.push([disk.length, size])
+      freeSlots.push([disk.length, size]);
       range(size).forEach(() => disk.push("."));
     }
   }
 
-  files.reverse()
+  files.reverse();
 
   for (const [f0, fsize] of files) {
     // find first free slot that is big enough for the current fileId's file size
-    const slotIndex = freeSlots.findIndex(([s0, size]) => size > 0 && fsize <= size && f0 > s0)
+    const slotIndex = freeSlots.findIndex(([s0, size]) =>
+      size > 0 && fsize <= size && f0 > s0
+    );
 
     // don't move the file (no free space for it)
     if (slotIndex === -1) {
-      continue
+      continue;
     }
 
     // console.log(disk.join('') + ' <- ' + disk[f0])
     // console.log(slotIndex, freeSlots)
-    const [s0, ssize] = freeSlots[slotIndex]
+    const [s0, ssize] = freeSlots[slotIndex];
 
     // write the fileId in the free slot
     for (let i = 0; i < fsize; i++) {
-      disk[s0 + i] = disk[f0 + i]
-      disk[f0 + i] = "."
+      disk[s0 + i] = disk[f0 + i];
+      disk[f0 + i] = ".";
     }
 
     // update the free slot map
-    freeSlots[slotIndex] = [s0 + fsize, ssize - fsize]
+    freeSlots[slotIndex] = [s0 + fsize, ssize - fsize];
   }
-
 
   console.log(
     sumAll(
@@ -111,11 +99,20 @@ export async function step2(input: string) {
 }
 
 if (import.meta.main) {
+  const file = await Deno.readTextFile(
+    path.join(import.meta.dirname, "input.txt"),
+  );
+
+  const bonus = await Deno.readTextFile(
+    path.join(import.meta.dirname, "bonus.txt"),
+  );
+
+  const example = "2333133121414131402";
+  const reddit = `233313312141413140211`;
+
   await step1(file);
   // 8632330985597 too high (1
   // 6476642796832 !!! (3
   // 5341160662740 too low (2
-
-  const reddit = `233313312141413140211`
   await step2(file);
 }
